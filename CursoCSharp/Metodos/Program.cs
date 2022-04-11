b7d24bfe019e75c41df77c23aaf44583facb87fc
AlxR8 <alejandro1605_14@hotmail.com>
2022-04-11 11:33:24 -0600

    Desafio Metodos1

---
+++ CursoCSharp/Metodos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string nombre1 = "Katherine";
+            string nombre2;
+            string nombre3;
+
+            SaludarAmigo(nombre1);
+            Console.Read();
+        }
+
+        public static void SaludarAmigo(string nombre)
+        {
+            Console.WriteLine("Hola {0}, que tenga un buen dia.", nombre);
+        }
+    }
+}
