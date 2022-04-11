fb61e42c86ce9112e22a5bdb4a6f4b42b3b195f6
AlxR8 <alejandro1605_14@hotmail.com>
2022-04-11 11:23:27 -0600

    Desafio String 1

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -10,6 +10,8 @@ namespace CursoCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadLine();
         }
     }
 }
---
+++ CursoCSharp/DesafioString1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioString1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string texto;
+
+            Console.WriteLine("Por favor, ingresa tu nombre y presiona enter: ");
+            texto = Console.ReadLine();
+
+            Console.WriteLine("El nombre ingresado es : " + texto);
+            Console.Read();
+
+        }
+    }
+}
